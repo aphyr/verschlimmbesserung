825f0ec87d8661735e9ec443d989a014e8c60ede
Aphyr <aphyr@aphyr.com>
2014-04-18 00:32:24 -0700

    0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject verschlimmbesserung "0.1.1-SNAPSHOT"
+(defproject verschlimmbesserung "0.1.1"
   :description "An etcd client."
   :url "https://github.com/aphyr/verschlimmbesserung"
   :license {:name "Eclipse Public License"
