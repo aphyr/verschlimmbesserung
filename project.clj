498fd20ca39c52f6f4506dc281d6af9920791342
Aphyr <aphyr@aphyr.com>
2016-07-20 10:38:02 -0500

    0.1.3

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject verschlimmbesserung "0.1.3-SNAPSHOT"
+(defproject verschlimmbesserung "0.1.3"
   :description "An etcd client."
   :url "https://github.com/aphyr/verschlimmbesserung"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cheshire "5.2.0"]
-                 [clj-http "0.7.7"]])
+  :dependencies [[cheshire "5.6.3"]
+                 [clj-http "2.2.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}})
